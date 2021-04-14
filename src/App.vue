b069b5c2dc896780bcb6d12352a7a106a498e03e
Pro-pro-cloud <Pro-pro-cloud>
2021-04-14 21:13:54 +0800

    基本功能实现

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
   <div id="app">
-
+    <tabbar>
+      <TabbarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+        <div slot="item-text">首页</div>
+      </TabbarItem>
+      <TabbarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
+        <div slot="item-text">分类</div>
+      </TabbarItem>
+      <TabbarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </TabbarItem>
+      <TabbarItem>
+        <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
+        <div slot="item-text">我的</div>
+      </TabbarItem>
+    </tabbar>
   </div>
 </template>
 
+<script>
+import Tabbar from './components/tabbar/Tabbar'
+import TabbarItem from './components/tabbar/TabbarItem'
+
+export default {
+  name: 'App',
+  components: {
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
+
 <style lang="scss">
+@import "./assets/css/base.css";
 
 </style>
---
+++ src/components/tabbar/TabbarItem.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="tab-bar-item">
+  <slot name="item-icon"></slot>
+  <slot name="item-text"></slot>
+  <!-- <img src="../../assets/img/tabbar/home.svg" alt="">
+  <div>首页</div> -->
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TabbarItem'
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  /* line-height: 49px; */
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  margin-bottom: 2px;
+  font-size: 14px;
+  vertical-align: middle;
+}
+</style>
