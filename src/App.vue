b7a2d4b87c04bf458b70afcc5116d4d6f44b9c00
Pro-pro-cloud <Pro-pro-cloud>
2021-04-16 17:54:52 +0800

    路由实现

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
+
+    <router-view></router-view>
+
     <tabbar>
-      <TabbarItem>
+      <TabbarItem path="/home">
         <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </TabbarItem>
-      <TabbarItem>
+      <TabbarItem path="/categery">
         <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </TabbarItem>
-      <TabbarItem>
+      <TabbarItem path="/cart">
         <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </TabbarItem>
-      <TabbarItem>
+      <TabbarItem path="/profile">
         <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
         <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
       </TabbarItem>
     </tabbar>
+
   </div>
 </template>
 
